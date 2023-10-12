058165a9890176604a010f7b3de433abc5b2858d
manleKim <http://github.com/manleKim>
2023-10-12 22:15:36 +0900

    initial setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:cafe_plug_guardian_client/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
