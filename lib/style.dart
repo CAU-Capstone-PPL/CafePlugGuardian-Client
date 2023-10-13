da81f620bd49447b153a5ac60ab868be595e7cea
manleKim <http://github.com/manleKim>
2023-10-13 17:08:28 +0900

    feat: colors and fonts setting

---
+++ lib/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor extends Color {
+  AppColor(super.value);
+
+  static const background = Color(0xFFF4F4F5);
+  static const main = Color(0xFF58E2C9);
+  static const text = Color(0xFF282828);
+}
