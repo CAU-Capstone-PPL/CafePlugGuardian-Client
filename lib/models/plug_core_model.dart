64c5365436d7377230b09b5a074aa6bb2713fa34
manleKim <kk410910@naver.com>
2023-11-07 22:34:32 +0900

    refactor: plug_core_model 간소화

--- lib/models/plug_core_model.dart
+++ lib/models/plug_core_model.dart
@@ -1,17 +1,10 @@
 class PlugCoreModel {
-  final String plugId,
-      plugName,
-      onOff,
-      startTime,
-      runningTime,
-      usedPower,
-      assignPower;
+  final String plugId, plugName, onOff, runningTime, usedPower, assignPower;
 
   PlugCoreModel.fromJson(Map<String, dynamic> json)
       : plugId = json['plugId'],
         plugName = json['plugName'],
         onOff = json['onOff'],
-        startTime = json['startTime'],
         runningTime = json['runningTime'],
         usedPower = json['usedPower'],
         assignPower = json['assignPower'];
