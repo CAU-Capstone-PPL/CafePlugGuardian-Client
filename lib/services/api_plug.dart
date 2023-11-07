88180d4b1fe00c626bdbcd560ecdfcc621af40c6
manleKim <kk410910@naver.com>
2023-11-07 17:39:05 +0900

    feat: add alert test api code

--- lib/services/api_plug.dart
+++ lib/services/api_plug.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'package:cafe_plug_guardian_client/models/alert_model.dart';
 import 'package:cafe_plug_guardian_client/models/plug_core_model.dart';
 import 'package:cafe_plug_guardian_client/models/plug_detail_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiPlug {
-  static const String baseUrl = "http://43.202.29.19";
+  static const String baseUrl = 'http://43.202.29.19';
 
   static Future<List<PlugCoreModel>> getPlugs() async {
     List<PlugCoreModel> plugInstances = [];
@@ -22,7 +23,7 @@ class ApiPlug {
   }
 
   static Future<PlugDetatilModel> getPlugById(String id) async {
-    final url = Uri.parse("$baseUrl/$id URL"); //url 수정 필요
+    final url = Uri.parse('$baseUrl/$id URL'); //url 수정 필요
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final plug = jsonDecode(response.body);
@@ -41,124 +42,139 @@ class ApiPlug {
     throw Error();
   }
 
+  static Future<List<AlertModel>> getAlertList() async {
+    List<AlertModel> alertInstance = [];
+    final url = Uri.parse('$baseUrl/alerts url'); //url 수정 필요
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> alerts = jsonDecode(response.body);
+      for (var alert in alerts) {
+        final instance = AlertModel.fromJson(alert);
+        alertInstance.add(instance);
+      }
+      return alertInstance;
+    }
+    throw Error();
+  }
+
   //더미데이터
   static const List<Map<String, dynamic>> dummyDataPlugCore = [
     {
-      "plugId": "1",
-      "plugName": "Plug 1",
-      "onOff": "On",
-      "startTime": "08:00 AM",
-      "runningTime": "2 h",
-      "usedPower": "100W",
-      "assignPower": "150W",
+      'plugId': '1',
+      'plugName': 'Plug 1',
+      'onOff': 'On',
+      'startTime': '08:00 AM',
+      'runningTime': '2 h',
+      'usedPower': '100W',
+      'assignPower': '150W',
     },
     {
-      "plugId": "2",
-      "plugName": "Plug 2",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
+      'plugId': '2',
+      'plugName': 'Plug 2',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
     },
     {
-      "plugId": "3",
-      "plugName": "Plug 3",
-      "onOff": "On",
-      "startTime": "03:45 PM",
-      "runningTime": "3 h",
-      "usedPower": "120W",
-      "assignPower": "200W",
+      'plugId': '3',
+      'plugName': 'Plug 3',
+      'onOff': 'On',
+      'startTime': '03:45 PM',
+      'runningTime': '3 h',
+      'usedPower': '120W',
+      'assignPower': '200W',
     },
     {
-      "plugId": "4",
-      "plugName": "Plug 4",
-      "onOff": "On",
-      "startTime": "09:15 AM",
-      "runningTime": "1.5 h",
-      "usedPower": "80W",
-      "assignPower": "110W"
+      'plugId': '4',
+      'plugName': 'Plug 4',
+      'onOff': 'On',
+      'startTime': '09:15 AM',
+      'runningTime': '1.5 h',
+      'usedPower': '80W',
+      'assignPower': '110W'
     },
     {
-      "plugId": "5",
-      "plugName": "Plug 5",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": ""
+      'plugId': '5',
+      'plugName': 'Plug 5',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': ''
     },
     {
-      "plugId": "6",
-      "plugName": "Plug 6",
-      "onOff": "On",
-      "startTime": "04:30 PM",
-      "runningTime": "2 h",
-      "usedPower": "110W",
-      "assignPower": "150W"
+      'plugId': '6',
+      'plugName': 'Plug 6',
+      'onOff': 'On',
+      'startTime': '04:30 PM',
+      'runningTime': '2 h',
+      'usedPower': '110W',
+      'assignPower': '150W'
     },
     {
-      "plugId": "7",
-      "plugName": "Plug 7",
-      "onOff": "On",
-      "startTime": "11:45 AM",
-      "runningTime": "2.5 h",
-      "usedPower": "95W",
-      "assignPower": "130W"
+      'plugId': '7',
+      'plugName': 'Plug 7',
+      'onOff': 'On',
+      'startTime': '11:45 AM',
+      'runningTime': '2.5 h',
+      'usedPower': '95W',
+      'assignPower': '130W'
     },
     {
-      "plugId": "8",
-      "plugName": "Plug 8",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": ""
+      'plugId': '8',
+      'plugName': 'Plug 8',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': ''
     },
     {
-      "plugId": "9",
-      "plugName": "Plug 9",
-      "onOff": "On",
-      "startTime": "06:00 AM",
-      "runningTime": "4 h",
-      "usedPower": "150W",
-      "assignPower": "180W"
+      'plugId': '9',
+      'plugName': 'Plug 9',
+      'onOff': 'On',
+      'startTime': '06:00 AM',
+      'runningTime': '4 h',
+      'usedPower': '150W',
+      'assignPower': '180W'
     },
     {
-      "plugId": "10",
-      "plugName": "Plug 10",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": ""
+      'plugId': '10',
+      'plugName': 'Plug 10',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': ''
     },
     {
-      "plugId": "11",
-      "plugName": "Plug 11",
-      "onOff": "On",
-      "startTime": "07:30 AM",
-      "runningTime": "3 h",
-      "usedPower": "120W",
-      "assignPower": "160W"
+      'plugId': '11',
+      'plugName': 'Plug 11',
+      'onOff': 'On',
+      'startTime': '07:30 AM',
+      'runningTime': '3 h',
+      'usedPower': '120W',
+      'assignPower': '160W'
     },
     {
-      "plugId": "12",
-      "plugName": "Plug 12",
-      "onOff": "On",
-      "startTime": "10:00 AM",
-      "runningTime": "1.5 h",
-      "usedPower": "70W",
-      "assignPower": "100W"
+      'plugId': '12',
+      'plugName': 'Plug 12',
+      'onOff': 'On',
+      'startTime': '10:00 AM',
+      'runningTime': '1.5 h',
+      'usedPower': '70W',
+      'assignPower': '100W'
     },
     {
-      "plugId": "13",
-      "plugName": "Plug 13",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": ""
+      'plugId': '13',
+      'plugName': 'Plug 13',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': ''
     }
   ];
 
@@ -176,134 +192,134 @@ class ApiPlug {
   //더미데이터
   static const List<Map<String, dynamic>> dummyDataPlugDetail = [
     {
-      "plugId": "1",
-      "plugName": "Plug 1",
-      "onOff": "On",
-      "startTime": "08:00 AM",
-      "runningTime": "2 h",
-      "usedPower": "100W",
-      "assignPower": "150W",
-      "plugDescription": "hello 1",
+      'plugId': '1',
+      'plugName': 'Plug 1',
+      'onOff': 'On',
+      'startTime': '08:00 AM',
+      'runningTime': '2 h',
+      'usedPower': '100W',
+      'assignPower': '150W',
+      'plugDescription': 'hello 1',
     },
     {
-      "plugId": "2",
-      "plugName": "Plug 2",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
-      "plugDescription": "hello 2",
+      'plugId': '2',
+      'plugName': 'Plug 2',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
+      'plugDescription': 'hello 2',
     },
     {
-      "plugId": "3",
-      "plugName": "Plug 3",
-      "onOff": "On",
-      "startTime": "03:45 PM",
-      "runningTime": "3 h",
-      "usedPower": "120W",
-      "assignPower": "200W",
-      "plugDescription": "hello 3",
+      'plugId': '3',
+      'plugName': 'Plug 3',
+      'onOff': 'On',
+      'startTime': '03:45 PM',
+      'runningTime': '3 h',
+      'usedPower': '120W',
+      'assignPower': '200W',
+      'plugDescription': 'hello 3',
     },
     {
-      "plugId": "4",
-      "plugName": "Plug 4",
-      "onOff": "On",
-      "startTime": "09:15 AM",
-      "runningTime": "1.5 h",
-      "usedPower": "80W",
-      "assignPower": "110W",
-      "plugDescription": "hello 4",
+      'plugId': '4',
+      'plugName': 'Plug 4',
+      'onOff': 'On',
+      'startTime': '09:15 AM',
+      'runningTime': '1.5 h',
+      'usedPower': '80W',
+      'assignPower': '110W',
+      'plugDescription': 'hello 4',
     },
     {
-      "plugId": "5",
-      "plugName": "Plug 5",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
-      "plugDescription": "hello 5",
+      'plugId': '5',
+      'plugName': 'Plug 5',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
+      'plugDescription': 'hello 5',
     },
     {
-      "plugId": "6",
-      "plugName": "Plug 6",
-      "onOff": "On",
-      "startTime": "04:30 PM",
-      "runningTime": "2 h",
-      "usedPower": "110W",
-      "assignPower": "150W",
-      "plugDescription": "hello 6",
+      'plugId': '6',
+      'plugName': 'Plug 6',
+      'onOff': 'On',
+      'startTime': '04:30 PM',
+      'runningTime': '2 h',
+      'usedPower': '110W',
+      'assignPower': '150W',
+      'plugDescription': 'hello 6',
     },
     {
-      "plugId": "7",
-      "plugName": "Plug 7",
-      "onOff": "On",
-      "startTime": "11:45 AM",
-      "runningTime": "2.5 h",
-      "usedPower": "95W",
-      "assignPower": "130W",
-      "plugDescription": "hello 7",
+      'plugId': '7',
+      'plugName': 'Plug 7',
+      'onOff': 'On',
+      'startTime': '11:45 AM',
+      'runningTime': '2.5 h',
+      'usedPower': '95W',
+      'assignPower': '130W',
+      'plugDescription': 'hello 7',
     },
     {
-      "plugId": "8",
-      "plugName": "Plug 8",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
-      "plugDescription": "hello 8",
+      'plugId': '8',
+      'plugName': 'Plug 8',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
+      'plugDescription': 'hello 8',
     },
     {
-      "plugId": "9",
-      "plugName": "Plug 9",
-      "onOff": "On",
-      "startTime": "06:00 AM",
-      "runningTime": "4 h",
-      "usedPower": "150W",
-      "assignPower": "180W",
-      "plugDescription": "hello 9",
+      'plugId': '9',
+      'plugName': 'Plug 9',
+      'onOff': 'On',
+      'startTime': '06:00 AM',
+      'runningTime': '4 h',
+      'usedPower': '150W',
+      'assignPower': '180W',
+      'plugDescription': 'hello 9',
     },
     {
-      "plugId": "10",
-      "plugName": "Plug 10",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
-      "plugDescription": "hello 10",
+      'plugId': '10',
+      'plugName': 'Plug 10',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
+      'plugDescription': 'hello 10',
     },
     {
-      "plugId": "11",
-      "plugName": "Plug 11",
-      "onOff": "On",
-      "startTime": "07:30 AM",
-      "runningTime": "3 h",
-      "usedPower": "120W",
-      "assignPower": "160W",
-      "plugDescription": "hello 11",
+      'plugId': '11',
+      'plugName': 'Plug 11',
+      'onOff': 'On',
+      'startTime': '07:30 AM',
+      'runningTime': '3 h',
+      'usedPower': '120W',
+      'assignPower': '160W',
+      'plugDescription': 'hello 11',
     },
     {
-      "plugId": "12",
-      "plugName": "Plug 12",
-      "onOff": "On",
-      "startTime": "10:00 AM",
-      "runningTime": "1.5 h",
-      "usedPower": "70W",
-      "assignPower": "100W",
-      "plugDescription": "hello 12",
+      'plugId': '12',
+      'plugName': 'Plug 12',
+      'onOff': 'On',
+      'startTime': '10:00 AM',
+      'runningTime': '1.5 h',
+      'usedPower': '70W',
+      'assignPower': '100W',
+      'plugDescription': 'hello 12',
     },
     {
-      "plugId": "13",
-      "plugName": "Plug 13",
-      "onOff": "Off",
-      "startTime": "",
-      "runningTime": "",
-      "usedPower": "",
-      "assignPower": "",
-      "plugDescription": "hello 13",
+      'plugId': '13',
+      'plugName': 'Plug 13',
+      'onOff': 'Off',
+      'startTime': '',
+      'runningTime': '',
+      'usedPower': '',
+      'assignPower': '',
+      'plugDescription': 'hello 13',
     }
   ];
 
@@ -328,5 +344,85 @@ class ApiPlug {
   static Future<List<double>> testGetWeeklyPower() async {
     List<double> weeklyPowerData = dummyDataWeeklyPowerData;
     return Future.delayed(const Duration(seconds: 0), () => weeklyPowerData);
+  }
+
+  //더미데이터
+  static const List<Map<String, dynamic>> dummyDataAlerts = [
+    {
+      'plugId': '1',
+      'plugName': 'Plug 1',
+      'blockingTime': '11-07 08:00 AM',
+    },
+    {
+      'plugId': '2',
+      'plugName': 'Plug 2',
+      'blockingTime': '11-08 09:30 AM',
+    },
+    {
+      'plugId': '3',
+      'plugName': 'Plug 3',
+      'blockingTime': '11-09 10:15 AM',
+    },
+    {
+      'plugId': '4',
+      'plugName': 'Plug 4',
+      'blockingTime': '11-10 11:45 AM',
+    },
+    {
+      'plugId': '5',
+      'plugName': 'Plug 5',
+      'blockingTime': '11-11 01:20 PM',
+    },
+    {
+      'plugId': '6',
+      'plugName': 'Plug 6',
+      'blockingTime': '11-12 02:45 PM',
+    },
+    {
+      'plugId': '7',
+      'plugName': 'Plug 7',
+      'blockingTime': '11-13 03:30 PM',
+    },
+    {
+      'plugId': '8',
+      'plugName': 'Plug 8',
+      'blockingTime': '11-14 04:10 PM',
+    },
+    {
+      'plugId': '9',
+      'plugName': 'Plug 9',
+      'blockingTime': '11-15 05:45 PM',
+    },
+    {
+      'plugId': '10',
+      'plugName': 'Plug 10',
+      'blockingTime': '11-16 07:00 PM',
+    },
+    {
+      'plugId': '11',
+      'plugName': 'Plug 11',
+      'blockingTime': '11-17 08:15 PM',
+    },
+    {
+      'plugId': '12',
+      'plugName': 'Plug 12',
+      'blockingTime': '11-18 09:30 PM',
+    },
+    {
+      'plugId': '13',
+      'plugName': 'Plug 13',
+      'blockingTime': '11-19 10:45 PM',
+    },
+  ];
+
+  static Future<List<AlertModel>> tsetGetAlertList() async {
+    // 실제 API를 호출하는 대신 더미 데이터를 반환합니다.
+    List<AlertModel> alertInstance = [];
+    const List<dynamic> alerts = dummyDataAlerts;
+    for (var alert in alerts) {
+      final instance = AlertModel.fromJson(alert);
+      alertInstance.add(instance);
+    }
+    return alertInstance;
   }
 }
