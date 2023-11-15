33faddb7d67440f76505cb5ef4cc4dd4402bd9fc
manleKim <kk410910@naver.com>
2023-11-15 21:49:42 +0900

    feat: 플러그 토글 on/off api 추가

--- lib/services/api_plug.dart
+++ lib/services/api_plug.dart
@@ -5,7 +5,7 @@ import 'package:cafe_plug_guardian_client/models/plug_detail_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiPlug {
-  static const String baseUrl = 'http://43.202.29.19';
+  static const String baseUrl = 'http://43.202.29.19/api';
 
   //플러그 정보 리스트 (get) ⇒ 홈스크린과 플러그 전체 스크린에 띄울 것
   static Future<List<PlugCoreModel>> getOnPlugs() async {
@@ -45,7 +45,27 @@ class ApiPlug {
 
   //플러그 총 갯수와 플러그 리스트 → 플러그 리스트 스크린에 띄울 것 (get) : static Future<List<PlugCoreModel>> getPlugs() async
 
+  //플러그 개별 제어 on/off → 홈스크린에서 개별 플러그 On/off 제어 (patch)
+  static Future<bool> patchPlugOn(int plugId) async {
+    final url = Uri.parse('$baseUrl/plug/$plugId/turnOn');
+    final response = await http.patch(url);
+
+    if (response.statusCode != 200) {
+      return false;
+    }
+    return true;
+  }
+
   //플러그 개별 제어 on/off → 홈스크린에서 개별 플러그 On/off 제어 (post)
+  static Future<bool> patchPlugOff(int plugId) async {
+    final url = Uri.parse('$baseUrl/plug/$plugId/turnOff');
+    final response = await http.patch(url);
+
+    if (response.statusCode != 200) {
+      return false;
+    }
+    return true;
+  }
 
   //최근 일주일 간 총 플러그 전력 사용량 (get)
   static Future<List<double>> getWeeklyPower() async {
