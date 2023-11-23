3b379bffd5d6b762d05e0e4e184a6f24ae847f30
manleKim <kk410910@naver.com>
2023-11-23 11:35:21 +0900

    feat: provider 연결

--- lib/screens/plug_list_screen.dart
+++ lib/screens/plug_list_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
+
 import 'package:cafe_plug_guardian_client/models/plug_core_model.dart';
+import 'package:cafe_plug_guardian_client/provider/plug_core_provider.dart';
 import 'package:cafe_plug_guardian_client/screens/plug_connect_screen.dart';
 import 'package:cafe_plug_guardian_client/services/api_plug.dart';
 import 'package:cafe_plug_guardian_client/style.dart';
 import 'package:cafe_plug_guardian_client/widgets/custom_button_widget.dart';
 import 'package:cafe_plug_guardian_client/widgets/plug_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlugListScreen extends StatefulWidget {
   const PlugListScreen({super.key});
@@ -16,11 +20,30 @@ class PlugListScreen extends StatefulWidget {
 class _PlugListScreenState extends State<PlugListScreen> {
   late final Future<List<PlugCoreModel>> plugs;
 
+  late Timer _timer;
+
   @override
   void initState() {
     super.initState();
-    //plugs = ApiTest.testGetPlugs();
-    plugs = ApiPlug.getPlugs();
+    context.read<PlugCoreProvider>().updatePlugs();
+    _startTimer();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      context.read<PlugCoreProvider>().updatePlugs();
+    });
+  }
+
+  void _stopTimer() {
+    _timer.cancel();
+  }
+
+  @override
+  void dispose() {
+    _stopTimer();
+    //_scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,12 +51,77 @@ class _PlugListScreenState extends State<PlugListScreen> {
     return Scaffold(
       backgroundColor: AppColor.background,
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: AppColor.background,
         title: const AppBarText(content: '플러그 관리 페이지'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            _stopTimer();
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: FutureBuilder(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                HeadingText(
+                    content:
+                        '총 개수: ${context.watch<PlugCoreProvider>().plugs.length}'),
+                Row(
+                  children: [
+                    CustomButton(
+                        content: '플러그 추가',
+                        onPressed: () {
+                          _stopTimer();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PlugConnectScreen(),
+                            ),
+                          ).then((_) {
+                            _startTimer();
+                          });
+                        }),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    CustomButton(content: '플러그 삭제', onPressed: () {}),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: GridView.count(
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 2,
+                childAspectRatio: 1.2,
+                children: context
+                    .watch<PlugCoreProvider>()
+                    .plugs
+                    .map(
+                      (plug) => Plug(
+                        plugId: plug.plugId,
+                        plugName: plug.plugName,
+                        onOff: plug.onOff,
+                        runningTime: plug.runningTime,
+                        usedPower: plug.usedPower,
+                        assignPower: plug.assignPower,
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ), /*FutureBuilder(
           future: plugs,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -64,13 +152,16 @@ class _PlugListScreenState extends State<PlugListScreen> {
                         CustomButton(
                             content: '플러그 추가',
                             onPressed: () {
+                              _stopTimer();
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) =>
                                       const PlugConnectScreen(),
                                 ),
-                              );
+                              ).then((_) {
+                                _startTimer();
+                              });
                             }),
                         const SizedBox(
                           width: 10,
@@ -104,7 +195,7 @@ class _PlugListScreenState extends State<PlugListScreen> {
               ],
             );
           },
-        ),
+        ),*/
       ),
     );
   }
