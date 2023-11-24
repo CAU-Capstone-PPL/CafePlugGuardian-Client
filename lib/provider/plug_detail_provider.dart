94dfccc045d9e91c972507f9b9c1e1344983ac13
manleKim <kk410910@naver.com>
2023-11-24 21:24:08 +0900

    modify: 플러그 상세 모델 변경으로 인한  플러그 상세 provider 변경

--- lib/provider/plug_detail_provider.dart
+++ lib/provider/plug_detail_provider.dart
@@ -1,10 +1,20 @@
 import 'package:cafe_plug_guardian_client/models/plug_detail_model.dart';
+import 'package:cafe_plug_guardian_client/models/time_model.dart';
 import 'package:cafe_plug_guardian_client/services/api_test.dart';
 import 'package:flutter/material.dart';
 
 class PlugDetailProvider extends ChangeNotifier {
-  PlugDetatilModel _plug =
-      PlugDetatilModel(plugId: 0, plugName: '', onOff: '', plugDescription: '');
+  PlugDetatilModel _plug = PlugDetatilModel(
+    plugId: 0,
+    plugName: '',
+    onOff: 'Off',
+    plugDescription: '로딩 중...',
+    startTime: TimeModel(hours: 0, minutes: 0),
+    runningTime: TimeModel(hours: 0, minutes: 0),
+    assignPower: 0.0,
+    usedPower: 0.0,
+    realTimePower: 0.0,
+  );
   bool _test = true;
 
   PlugDetatilModel? get plug => _plug;
