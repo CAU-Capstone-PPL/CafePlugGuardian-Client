f62ce82a7ecbfa584db8cb02d6572862008081a8
manleKim <kk410910@naver.com>
2023-11-25 23:30:24 +0900

    feat: 파이어베이스 연동

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'package:cafe_plug_guardian_client/screens/login_screen.dart';
 import 'package:cafe_plug_guardian_client/style.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
