889cb2c7ea45f5419982a418fe090d78bd586b8c
manleKim <kk410910@naver.com>
2023-11-26 14:32:22 +0900

    modify: blockingTime 타입을 DateTimeModel로 변경

--- lib/models/alert_model.dart
+++ lib/models/alert_model.dart
@@ -1,9 +1,14 @@
+import 'package:cafe_plug_guardian_client/models/date_time_model.dart';
+
 class AlertModel {
-  final String plugName, blockingTime;
   final int plugId;
+  final String plugName;
+  final DateTimeModel blockingTime;
+  final bool check;
 
   AlertModel.fromJson(Map<String, dynamic> json)
       : plugId = json['plugId'],
         plugName = json['plugName'],
-        blockingTime = json['blockingTime'];
+        blockingTime = DateTimeModel.fromJson(json['blockingTime']),
+        check = json['check'];
 }
