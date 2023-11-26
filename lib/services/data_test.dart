abbffa82b6768ab08f04e38cc783479a428ac07a
manleKim <kk410910@naver.com>
2023-11-26 15:04:14 +0900

    modify: 알람 더미 데이터 변경

--- lib/services/data_test.dart
+++ lib/services/data_test.dart
@@ -516,69 +516,76 @@ const List<Map<String, dynamic>> dummyDataChanged = [
 //더미데이터
 const List<Map<String, dynamic>> dummyDataAlerts = [
   {
-    'plugId': 1,
-    'plugName': 'Plug 1',
-    'blockingTime': '11-07 08:00 AM',
+    "plugId": 101,
+    "plugName": "Plug101",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 26},
+      "time": {"hours": 20, "minutes": 31},
+    },
+    "check": false
   },
   {
-    'plugId': 2,
-    'plugName': 'Plug 2',
-    'blockingTime': '11-08 09:30 AM',
+    "plugId": 202,
+    "plugName": "Plug202",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 25},
+      "time": {"hours": 18, "minutes": 45}
+    },
+    "check": true
   },
   {
-    'plugId': 3,
-    'plugName': 'Plug 3',
-    'blockingTime': '11-09 10:15 AM',
+    "plugId": 303,
+    "plugName": "Plug303",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 24},
+      "time": {"hours": 16, "minutes": 12}
+    },
+    "check": false
   },
   {
-    'plugId': 4,
-    'plugName': 'Plug 4',
-    'blockingTime': '11-10 11:45 AM',
+    "plugId": 404,
+    "plugName": "Plug404",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 23},
+      "time": {"hours": 14, "minutes": 28}
+    },
+    "check": true
   },
   {
-    'plugId': 5,
-    'plugName': 'Plug 5',
-    'blockingTime': '11-11 01:20 PM',
+    "plugId": 505,
+    "plugName": "Plug505",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 22},
+      "time": {"hours": 12, "minutes": 54}
+    },
+    "check": false
   },
   {
-    'plugId': 6,
-    'plugName': 'Plug 6',
-    'blockingTime': '11-12 02:45 PM',
+    "plugId": 606,
+    "plugName": "Plug606",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 21},
+      "time": {"hours": 11, "minutes": 10}
+    },
+    "check": true
   },
   {
-    'plugId': 7,
-    'plugName': 'Plug 7',
-    'blockingTime': '11-13 03:30 PM',
+    "plugId": 707,
+    "plugName": "Plug707",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 20},
+      "time": {"hours": 9, "minutes": 26}
+    },
+    "check": false
   },
   {
-    'plugId': 8,
-    'plugName': 'Plug 8',
-    'blockingTime': '11-14 04:10 PM',
-  },
-  {
-    'plugId': 9,
-    'plugName': 'Plug 9',
-    'blockingTime': '11-15 05:45 PM',
-  },
-  {
-    'plugId': 10,
-    'plugName': 'Plug 10',
-    'blockingTime': '11-16 07:00 PM',
-  },
-  {
-    'plugId': 11,
-    'plugName': 'Plug 11',
-    'blockingTime': '11-17 08:15 PM',
-  },
-  {
-    'plugId': 12,
-    'plugName': 'Plug 12',
-    'blockingTime': '11-18 09:30 PM',
-  },
-  {
-    'plugId': 13,
-    'plugName': 'Plug 13',
-    'blockingTime': '11-19 10:45 PM',
+    "plugId": 808,
+    "plugName": "Plug808",
+    "blockingTime": {
+      "date": {"year": 2023, "month": 11, "day": 19},
+      "time": {"hours": 7, "minutes": 42}
+    },
+    "check": true
   },
 ];
 
